0e8865364a55bb5ee0288649fa3d307d253af9cb
Neco_Stack <necoabay@googlemail.com>
2024-09-29 18:51:37 +0200

    string-methods-1_1

---
+++ src/String-Methods-TS-Level-1_1/basic.ts
@@ -0,0 +1,16 @@
+// Strings deklarieren und Länge anzeigen 
+let firstName:string = "Necati"; 
+let lastName:string = "Abay";
+console.log(firstName.length);
+console.log(lastName.length);
+// concat-Methode
+let fullName:string = firstName.concat(' ', lastName);
+console.log(fullName);
+console.log(fullName.length);
+
+
+
+
+
+
+
